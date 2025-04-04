b3b8815174252c87b9fd4bbe65bc6beb980d4f71
Pizzel <220584@umons.ac.be>
2025-04-04 17:07:18 +0200

    refactor: removed unused import

--- src/main/java/calculator/operation/BuildUnaryOperationFunction.java
+++ src/main/java/calculator/operation/BuildUnaryOperationFunction.java
@@ -4,7 +4,6 @@ import calculator.Expression;
 import calculator.IllegalConstruction;
 import calculator.operation.unary.UnaryOperation;
 
-import java.util.ArrayList;
 
 /**
  * An interface designed to make it easy to pass a unary operation as the result of a lambda function.
