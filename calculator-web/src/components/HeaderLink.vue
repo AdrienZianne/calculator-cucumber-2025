d1a7c9748900d9cfbd5d8916cef4c79ea1ec77a9
adrien <221212@umons.ac.be>
2025-04-28 22:52:54 +0200

    add missing component

---
+++ calculator-web/src/components/HeaderLink.vue
@@ -0,0 +1,34 @@
+<template>
+    <RouterLink :to="props.to" class="link">
+        <div>
+            {{ props.text }}
+        </div>
+    </RouterLink>
+</template>
+
+<script setup>
+import { RouterLink, useRouter } from 'vue-router';
+const router = useRouter();
+const props = defineProps({
+    to: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+});
+</script>
+
+<style>
+
+.link {
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    padding: 0 20px;
+}
+
+</style>
