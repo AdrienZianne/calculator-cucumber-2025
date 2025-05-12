dd6d95eba86346783e16fe297b85d9aebbfbf8de
fadri14 <fadri1@proton.me>
2025-05-12 11:50:08 +0200

    test(Memory): Improved understanding of unit tests

--- src/test/java/io/TestMemory.java
+++ src/test/java/io/TestMemory.java
@@ -13,19 +13,19 @@ import calculator.Configuration;
  * TestMemory
  * Class used to test the program's Memory class and its internal Element class.
  */
-public class TestMemory {
+class TestMemory {
 
     /**
      * Class used to test the correct behavior of the internal Element class.
      */
     @Test
-    public void testElement() {
+    void testElement() {
         Element e = new Element();
         e.setExpression("1 + 1");
         e.setRes("2");
-        assertEquals(e.getExpression(), "1 + 1");
-        assertEquals(e.getRes(), "2");
-        assertEquals(e.toString(), "1 + 1 = 2");
+        assertEquals("1 + 1", e.getExpression());
+        assertEquals("2", e.getRes());
+        assertEquals("1 + 1 = 2", e.toString());
 
         assertTrue(e.equals(e));
         assertTrue(e.equals(new Element("1 + 1", "2")));
@@ -33,7 +33,7 @@ public class TestMemory {
         assertFalse(e.equals(new Memory()));
 
         e = new Element("T and T", "T");
-        assertEquals(e.toString(), "T and T = T");
+        assertEquals("T and T = T", e.toString());
     }
 
     /**
@@ -41,7 +41,7 @@ public class TestMemory {
      * data, whether logs or favorites.
      */
     @Test
-    public void testMemory() {
+    void testMemory() {
         // Reset stored data to zero.
         Memory m = new Memory();
         m.reset(Category.LOG);
@@ -55,31 +55,31 @@ public class TestMemory {
         // Test on logs.
         m.addElement(Category.LOG, e1);
         m.addElement(Category.LOG, e2.getExpression(), e2.getRes());
-        assertEquals(m.getExpression(Category.LOG, 0), e1.getExpression());
-        assertEquals(m.getExpression(Category.LOG, 1), e2.getExpression());
+        assertEquals(e1.getExpression(), m.getExpression(Category.LOG, 0));
+        assertEquals(e2.getExpression(), m.getExpression(Category.LOG, 1));
 
         // Favourites test.
         m.addFavo(null);
-        assertEquals(m.getExpression(Category.FAVO, 0), e2.getExpression());
+        assertEquals(e2.getExpression(), m.getExpression(Category.FAVO, 0));
         m.addFavo(-1);
-        assertEquals(m.getExpression(Category.FAVO, 0), e2.getExpression());
+        assertEquals(e2.getExpression(), m.getExpression(Category.FAVO, 0));
         m.addFavo(10);
-        assertEquals(m.getExpression(Category.FAVO, 0), e2.getExpression());
+        assertEquals(e2.getExpression(), m.getExpression(Category.FAVO, 0));
         m.addFavo(0);
-        assertEquals(m.getExpression(Category.FAVO, 1), e1.getExpression());
+        assertEquals(e1.getExpression(), m.getExpression(Category.FAVO, 1));
         m.addFavo(1);
-        assertEquals(m.getExpression(Category.FAVO, 2), e2.getExpression());
+        assertEquals(e2.getExpression(), m.getExpression(Category.FAVO, 2));
         m.delFavo(null);
         m.addFavo(0);
-        assertEquals(m.getExpression(Category.FAVO, 2), e1.getExpression());
+        assertEquals(e1.getExpression(), m.getExpression(Category.FAVO, 2));
         m.delFavo(-1);
-        assertEquals(m.getExpression(Category.FAVO, 2), e1.getExpression());
+        assertEquals(e1.getExpression(), m.getExpression(Category.FAVO, 2));
         m.delFavo(10);
-        assertEquals(m.getExpression(Category.FAVO, 2), e1.getExpression());
+        assertEquals(e1.getExpression(), m.getExpression(Category.FAVO, 2));
     }
 
     @Test
-    public void testGetExpression() {
+    void testGetExpression() {
         Memory m = new Memory();
         m.reset(Category.LOG);
 
@@ -88,14 +88,14 @@ public class TestMemory {
         m.addElement(Category.LOG, e1);
         m.addElement(Category.LOG, e2);
 
-        assertEquals(m.getExpression(Category.LOG, null), e2.getExpression());
-        assertEquals(m.getExpression(Category.LOG, -1), "");
-        assertEquals(m.getExpression(Category.LOG, 10), "");
-        assertEquals(m.getExpression(Category.LOG, 0), e1.getExpression());
+        assertEquals(e2.getExpression(), m.getExpression(Category.LOG, null));
+        assertEquals("", m.getExpression(Category.LOG, -1));
+        assertEquals("", m.getExpression(Category.LOG, 10));
+        assertEquals(e1.getExpression(), m.getExpression(Category.LOG, 0));
     }
 
     @Test
-    public void testDeleteDuplicates() {
+    void testDeleteDuplicates() {
         // Reset stored data to zero.
         Memory m = new Memory();
         m.reset(Category.FAVO);
@@ -105,21 +105,21 @@ public class TestMemory {
 
         m.addElement(Category.FAVO, e1);
         m.addElement(Category.FAVO, e2);
-        assertEquals(m.getExpression(Category.FAVO, 1), e2.getExpression());
+        assertEquals(e2.getExpression(), m.getExpression(Category.FAVO, 1));
         m.delFavo(1);
         Configuration.setDeleteDuplicates(true);
         m.addElement(Category.FAVO, e1);
         m.addElement(Category.FAVO, e2);
-        assertEquals(m.getExpression(Category.FAVO, 1), e2.getExpression());
+        assertEquals(e2.getExpression(), m.getExpression(Category.FAVO, 1));
         m.delFavo(1);
         Configuration.setDeleteDuplicates(false);
         m.addElement(Category.FAVO, e1);
         m.addElement(Category.FAVO, e2);
-        assertEquals(m.getExpression(Category.FAVO, 1), e1.getExpression());
+        assertEquals(e1.getExpression(), m.getExpression(Category.FAVO, 1));
     }
 
     @Test
-    public void testMaxStore() {
+    void testMaxStore() {
         // Reset stored data to zero.
         Memory m = new Memory();
         m.reset(Category.FAVO);
@@ -129,12 +129,12 @@ public class TestMemory {
 
         Configuration.setMaxStore(1);
         m.addElement(Category.FAVO, e1);
-        assertEquals(m.getExpression(Category.FAVO, 0), e1.getExpression());
+        assertEquals(e1.getExpression(), m.getExpression(Category.FAVO, 0));
         m.addElement(Category.FAVO, e2);
-        assertEquals(m.getExpression(Category.FAVO, 0), e2.getExpression());
+        assertEquals(e2.getExpression(), m.getExpression(Category.FAVO, 0));
         Configuration.setMaxStore(2);
         m.addElement(Category.FAVO, e1);
-        assertEquals(m.getExpression(Category.FAVO, 0), e2.getExpression());
-        assertEquals(m.getExpression(Category.FAVO, 1), e1.getExpression());
+        assertEquals(e2.getExpression(), m.getExpression(Category.FAVO, 0));
+        assertEquals(e1.getExpression(), m.getExpression(Category.FAVO, 1));
     }
 }
